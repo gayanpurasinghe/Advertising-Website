4a461a2f7221ee8d0868b58a18216612a97b50ec
gayanpurasinghe <gayanpurasinghe.edu@gmail.com>
2026-01-15 15:46:51 +0530

    Adds footer with scroll-to-top button

---
+++ views/layout/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>View Ads</title>
+    <link rel="stylesheet" type="text/css" href="footer.css">
+</head>
+
+<body>
+
+    <div class="footer">
+        <p>&copy; 2024 BuySel. All rights reserved.</p>
+    </div>
+    <div class="scroll-top">
+        <a href="#top"><img src="..\..\assets\images\up-arrow.png" alt="Scroll to Top" class="up-arrow-icon"></a>
+    </div>
+</body>
+
+</html>
